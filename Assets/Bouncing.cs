d7472eb646a980d4c7813b199f10001c74315db0
d-Dudas <david.dudas03@e-uvt.ro>
2024-03-10 23:26:26 +0200

    First commit.

---
+++ Assets/Bouncing.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncing : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "floor")
+        {
+            Debug.Log("Landed on plane");
+            rb.AddForce(force);
+        }
+    }
+}
