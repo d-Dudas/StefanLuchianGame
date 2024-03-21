3918b21c0400d898c2b2085e8863f5d34eb24fd7
d-Dudas <david.dudas03@e-uvt.ro>
2024-03-21 11:05:10 +0200

    Updated camera controller with view angle limits and view change.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,6 +15,10 @@ public class CameraController : MonoBehaviour
     private CameraView currentView = CameraView.firstPersonView;
 
     private bool isVKeyPressed = false;
+    private const int maxLowerViewAngleLimit = 50;
+    private const int minLowerViewAngleLimit = -1;
+    private const int maxUpperViewAngleLimit = 360;
+    private const int minUpperViewAngleLimit = 290;
 
     private Keyboard keyboard;
 
@@ -24,15 +29,22 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        if(keyboard.numpad8Key.isPressed && currentView != CameraView.thirdPersonView)
+        if (keyboard.numpad8Key.isPressed && currentView != CameraView.thirdPersonView)
         {
-            transform.Rotate(new Vector3(-1, 0, 0));
+            
+            if (CanLookUp())
+            {
+                transform.Rotate(new Vector3(-1, 0, 0));
+            }
         }
-        else if(keyboard.numpad2Key.isPressed && currentView != CameraView.thirdPersonView)
+        else if (keyboard.numpad2Key.isPressed && currentView != CameraView.thirdPersonView)
         {
-            transform.Rotate(new Vector3(1, 0, 0));
+            if (CanLookDown())
+            {
+                transform.Rotate(new Vector3(1, 0, 0));
+            }
         }
-        if(keyboard.vKey.wasPressedThisFrame && !isVKeyPressed)
+        if (keyboard.vKey.wasPressedThisFrame && !isVKeyPressed)
         {
             isVKeyPressed = true;
             ChangeView();
@@ -43,17 +55,41 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    private bool CanLookUp()
+    {
+        int currentViewAngle = (int)transform.eulerAngles.x;
+        return (currentViewAngle <= maxLowerViewAngleLimit && currentViewAngle >= minLowerViewAngleLimit) || 
+                (currentViewAngle > minUpperViewAngleLimit && currentViewAngle <= maxUpperViewAngleLimit);
+    }
+
+    private bool CanLookDown()
+    {
+        int currentViewAngle = (int)transform.eulerAngles.x;
+        return (currentViewAngle < maxLowerViewAngleLimit && currentViewAngle >= minLowerViewAngleLimit) ||
+                (currentViewAngle >= minUpperViewAngleLimit - 1 && currentViewAngle <= maxUpperViewAngleLimit);
+    }
+
     private void ChangeView()
     {
-        if(currentView == CameraView.firstPersonView)
+        if (currentView == CameraView.firstPersonView)
         {
             currentView = CameraView.thirdPersonView;
+            transform.eulerAngles = new Vector3(
+                20,
+                transform.eulerAngles.y,
+                transform.eulerAngles.z
+            );
             transform.position -= transform.forward * 4f;
         }
         else
         {
             currentView = CameraView.firstPersonView;
             transform.position += transform.forward * 4f;
+            transform.eulerAngles = new Vector3(
+                0,
+                transform.eulerAngles.y,
+                transform.eulerAngles.z
+            );
         }
     }
 }
