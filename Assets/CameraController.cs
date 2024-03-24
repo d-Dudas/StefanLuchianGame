4a0ff9e218f0560751dd61fcc5a07d6ad5873760
David <93072019+d-Dudas@users.noreply.github.com>
2024-03-24 13:32:17 +0200

    Implemented camera controller and interactive objects script (#1)
    * Updated camera controller with view angle limits and view change.
    
    * Updated camera view adjustments on view change or wall hit.
    
    * Added a cursor in first persion view.
    
    * Added OpenableDoor script, with IInteractiveObjects interface and hover message.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,6 +15,16 @@ public class CameraController : MonoBehaviour
     private CameraView currentView = CameraView.firstPersonView;
 
     private bool isVKeyPressed = false;
+    private bool isEnterKeyPressed = false;
+    private const int maxLowerViewAngleLimit = 50;
+    private const int minLowerViewAngleLimit = -1;
+    private const int maxUpperViewAngleLimit = 360;
+    private const int minUpperViewAngleLimit = 290;
+    private const int minimumInteractionDistanceUnit = 2;
+
+    public GameObject cursor;
+
+    public Transform head;
 
     private Keyboard keyboard;
 
@@ -24,15 +35,22 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        if(keyboard.numpad8Key.isPressed && currentView != CameraView.thirdPersonView)
+        if (keyboard.numpad8Key.isPressed && currentView != CameraView.thirdPersonView)
         {
-            transform.Rotate(new Vector3(-1, 0, 0));
+
+            if (CanLookUp())
+            {
+                transform.Rotate(new Vector3(-1, 0, 0));
+            }
         }
-        else if(keyboard.numpad2Key.isPressed && currentView != CameraView.thirdPersonView)
+        else if (keyboard.numpad2Key.isPressed && currentView != CameraView.thirdPersonView)
         {
-            transform.Rotate(new Vector3(1, 0, 0));
+            if (CanLookDown())
+            {
+                transform.Rotate(new Vector3(1, 0, 0));
+            }
         }
-        if(keyboard.vKey.wasPressedThisFrame && !isVKeyPressed)
+        if (keyboard.vKey.wasPressedThisFrame && !isVKeyPressed)
         {
             isVKeyPressed = true;
             ChangeView();
@@ -41,19 +59,115 @@ public class CameraController : MonoBehaviour
         {
             isVKeyPressed = false;
         }
+        if(keyboard.enterKey.wasPressedThisFrame && !isEnterKeyPressed && currentView == CameraView.firstPersonView)
+        {
+            isEnterKeyPressed = true;
+            ObjectRaycast();
+        }
+        else if(keyboard.enterKey.wasReleasedThisFrame)
+        {
+            isEnterKeyPressed = false;
+        }
+        if(currentView == CameraView.firstPersonView)
+        {
+            ObjectHover();
+        }
+
+    }
+
+    private bool CanLookUp()
+    {
+        int currentViewAngle = (int)transform.eulerAngles.x;
+        return (currentViewAngle <= maxLowerViewAngleLimit && currentViewAngle >= minLowerViewAngleLimit) ||
+                (currentViewAngle > minUpperViewAngleLimit && currentViewAngle <= maxUpperViewAngleLimit);
+    }
+
+    private bool CanLookDown()
+    {
+        int currentViewAngle = (int)transform.eulerAngles.x;
+        return (currentViewAngle < maxLowerViewAngleLimit && currentViewAngle >= minLowerViewAngleLimit) ||
+                (currentViewAngle >= minUpperViewAngleLimit - 1 && currentViewAngle <= maxUpperViewAngleLimit);
     }
 
     private void ChangeView()
     {
-        if(currentView == CameraView.firstPersonView)
+        if (currentView == CameraView.firstPersonView)
         {
+            cursor.SetActive(false);
             currentView = CameraView.thirdPersonView;
+            transform.eulerAngles = new Vector3(
+                20,
+                transform.eulerAngles.y,
+                transform.eulerAngles.z
+            );
             transform.position -= transform.forward * 4f;
         }
         else
         {
+            AdjustCameraBeforeViewChange();
+            cursor.SetActive(true);
             currentView = CameraView.firstPersonView;
-            transform.position += transform.forward * 4f;
+            transform.position += transform.forward * 4.3f;
+            transform.eulerAngles = new Vector3(
+                0,
+                transform.eulerAngles.y,
+                transform.eulerAngles.z
+            );
+        }
+    }
+
+    private void AdjustCameraBeforeViewChange()
+    {
+        float desiredDistance = 4.0f;
+        Vector3 desiredCameraPosition = head.position - transform.forward * desiredDistance;
+        transform.position = desiredCameraPosition;
+    }
+
+    void LateUpdate()
+    {
+        if (currentView == CameraView.thirdPersonView)
+        {
+            AdjustCameraIfItHitsAWall();
+        }
+    }
+
+    private void AdjustCameraIfItHitsAWall()
+    {
+        float desiredDistance = 4.0f;
+        Vector3 desiredCameraPosition = head.position - transform.forward * desiredDistance;
+
+        if (Physics.Raycast(head.position, -transform.forward, out RaycastHit hit, desiredDistance))
+        {
+            transform.position = Vector3.Lerp(transform.position, head.position - transform.forward * hit.distance, 20f * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, desiredCameraPosition, 20f * Time.deltaTime);
+        }
+    }
+
+    private void ObjectRaycast()
+    {
+        Ray ray = new(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, minimumInteractionDistanceUnit))
+        {
+            hitInfo.transform.gameObject.SendMessage("Interact", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void ObjectHover()
+    {
+        Ray ray = new(transform.position, transform.forward);
+
+        if(Physics.Raycast(ray, out RaycastHit hit, minimumInteractionDistanceUnit))
+        {
+            Debug.Log("Looking for object");
+            if (hit.transform.TryGetComponent<IInteractiveObject>(out var interactiveUI))
+            {
+                Debug.Log("Object found");
+                interactiveUI.HandleHoverEnter();
+            }
         }
     }
 }
---
+++ Assets/OpenableDoor.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+
+public class OpenableDoor : MonoBehaviour, IInteractiveObject
+{
+    public Transform playerTransform;
+    private bool isDoorOpened = false;
+
+    private const int closedDoorAngle = 0;
+    private const int openedDoorAngle = 90;
+    private const float doorOpeningAnimationDuration = 0.5f;
+
+    private const float hoverTextDuration = 0.2f;
+    private float lastHover;
+
+    public GameObject frontText;
+    public GameObject backText;
+    
+    void Start()
+    {
+        frontText.SetActive(false);
+        backText.SetActive(false);
+    }
+
+    void Update()
+    {
+        lastHover -= Time.deltaTime;
+
+        if(lastHover <= 0.0f)
+        {
+            HandleHoverExit();
+        }
+    }
+
+    public void Interact()
+    {
+        int openedDoorRelativeAngle = openedDoorAngle * (ShouldOpenClockwise() ? -1 : 1);
+        StartCoroutine(ChangeDoorAngle(isDoorOpened ? closedDoorAngle : openedDoorRelativeAngle));
+        isDoorOpened = !isDoorOpened;
+    }
+
+    private bool ShouldOpenClockwise()
+    {
+        Vector3 toPlayer = playerTransform.position - transform.position;
+        float dot = Vector3.Dot(transform.forward, toPlayer);
+
+        return dot > 0;
+    }
+
+    private IEnumerator ChangeDoorAngle(int angle)
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(transform.eulerAngles.x, angle, transform.eulerAngles.z);
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < doorOpeningAnimationDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, elapsedTime / doorOpeningAnimationDuration);
+            yield return null;
+        }
+    }
+
+    public void HandleHoverEnter()
+    {
+        if(ShouldOpenClockwise())
+        {
+            frontText.SetActive(true);
+        }
+        else
+        {
+            backText.SetActive(true);
+        }
+        
+        lastHover = hoverTextDuration;
+    }
+
+    public void HandleHoverExit()
+    {
+        frontText.SetActive(false);
+        backText.SetActive(false);
+    }
+}
